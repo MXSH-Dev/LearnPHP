c28cea2cf75abaeb78bc1d83759b5d694f084d79
mxsh-dev <x597653559@gmail.com>
2021-01-16 15:18:12 -0700

    example to check if website is online or not

--- CoreMVC/public/index.php
+++ CoreMVC/public/index.php
@@ -12,4 +12,34 @@ require_once '../app/bootstrap.php';
 
 $init = new Core();
 
+
+// check website live example
+
+// function checkOnline($domain) {
+//     $curlInit = curl_init($domain);
+//     curl_setopt($curlInit,CURLOPT_CONNECTTIMEOUT,10);
+//     curl_setopt($curlInit,CURLOPT_HEADER,true);
+//     curl_setopt($curlInit,CURLOPT_NOBODY,true);
+//     curl_setopt($curlInit,CURLOPT_RETURNTRANSFER,true);
+
+//     //get answer
+//     $response = curl_exec($curlInit);
+
+//     curl_close($curlInit);
+
+//     echo $response;
+
+//     if ($response) return true;
+//     return false;
+// }
+
+// if(checkOnline('http://angular.io')) { echo "yes"; }
+
+// $header_check = get_headers("http://www.google.com");
+// $response_code = $header_check[0];
+
+// echo "\n".$response_code;
+
+// print_r($header_check);
+
 ?>
